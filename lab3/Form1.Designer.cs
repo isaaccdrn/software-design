2aee9f28f65788e5157689b33b1905207324c38c
isaacntnt <152797177+isaacntnt@users.noreply.github.com>
2026-02-05 11:24:49 +0800

    finished all

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,69 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            booksLBox = new MaterialSkin.Controls.MaterialListBox();
+            btnShowInfo = new MaterialSkin.Controls.MaterialButton();
+            SuspendLayout();
+            // 
+            // booksLBox
+            // 
+            booksLBox.BackColor = Color.White;
+            booksLBox.BorderColor = Color.LightGray;
+            booksLBox.Depth = 0;
+            booksLBox.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            booksLBox.Location = new Point(265, 67);
+            booksLBox.MouseState = MaterialSkin.MouseState.HOVER;
+            booksLBox.Name = "booksLBox";
+            booksLBox.SelectedIndex = -1;
+            booksLBox.SelectedItem = null;
+            booksLBox.Size = new Size(529, 377);
+            booksLBox.TabIndex = 0;
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnShowInfo.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            btnShowInfo.Depth = 0;
+            btnShowInfo.HighEmphasis = true;
+            btnShowInfo.Icon = null;
+            btnShowInfo.Location = new Point(61, 168);
+            btnShowInfo.Margin = new Padding(4, 6, 4, 6);
+            btnShowInfo.MouseState = MaterialSkin.MouseState.HOVER;
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.NoAccentTextColor = Color.Empty;
+            btnShowInfo.Size = new Size(158, 36);
+            btnShowInfo.TabIndex = 1;
+            btnShowInfo.Text = "Show Info";
+            btnShowInfo.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            btnShowInfo.UseAccentColor = false;
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnShowInfo_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnShowInfo);
+            Controls.Add(booksLBox);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Bookstore System";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private MaterialSkin.Controls.MaterialListBox booksLBox;
+        private MaterialSkin.Controls.MaterialButton btnShowInfo;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,70 @@
+using MaterialSkin.Controls;
+
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Book
+        {
+            required public string Title { get; set; }
+            public string? Author { get; set; }
+            public virtual string GetInfo() => $"{Title} by {Author}";
+        }
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+            public override string GetInfo() => $"{Title} (Issue #{IssueNumber})";
+        }
+
+        public class Ebook : Book
+        {
+            public float Rating { get; set; }
+            public override string GetInfo() => $"{Title} - Rating: {Rating}/5";
+        }
+
+        public class Textbook : Book
+        {
+            required public string Subject { get; set; }
+            public override string GetInfo() => $"{Title} by {Author} [Subject: {Subject}] ";
+        }
+
+
+        public class AudioBook : Book
+        {
+            required public string Duration { get; set; }
+            required public string Narrator { get; set; }
+            public override string GetInfo() => $"{Title} (Audiobook) - Narrated by {Narrator} [{Duration}]";
+        }
+
+        private void ProcessBooks(Book[] inventory)
+        {
+            booksLBox.Items.Clear();
+
+            foreach (Book b in inventory)
+            {
+                booksLBox.AddItem(b.GetInfo());
+            }
+        }
+
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            Book[] myInventory =
+            {
+                new Book { Title = "Fahrenheit 451", Author = "Ray Bradbury" },
+                new Magazine { Title = "Tech Weekly", IssueNumber = 45 },
+                new Ebook { Title = "Atomic Habits", Rating = 4.8f },
+                new Textbook { Title = "University Physics", Author = "Young and Freedman", Subject = "Science" },
+                new AudioBook { Title = "Crime and Punishment", Narrator = "Isaac Nathan", Duration = "24h 10m" }
+            };
+
+            // Call the polymorphic function
+            ProcessBooks(myInventory);
+        }
+
+    }
 }
